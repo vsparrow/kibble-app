bfc752c6ba869ebc7da3e89c73d2412e30e4a4f8
vsparrow.com <khan@vsparrow.com>
2020-02-04 01:43:59 +0000

    add component ShowKibble

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from './components/Navigation'
 import AddKibbleForm from './components/AddKibbleForm'
+import ShowKibble from './components/ShowKibble'
 
 const App = (props) => {
 	return (
@@ -8,6 +9,7 @@ const App = (props) => {
 			<Navigation />
 			Kibble
 			<AddKibbleForm />
+			<ShowKibble />
 		</div>
 	)
 }
---
+++ src/components/ShowKibble.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowKibble = props => {
+	return(
+		<div>
+		 Show Kibble info here
+		</div>
+	)
+}
+
+export default ShowKibble
