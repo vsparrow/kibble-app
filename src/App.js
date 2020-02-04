7c5f891ffb548b54bbd49ce9af8416a5a1ecc9e9
vsparrow.com <khan@vsparrow.com>
2020-02-04 19:56:11 +0000

    Add routing to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navigation from './components/Navigation'
 import AddKibbleForm from './components/AddKibbleForm'
 import ShowKibbles from './components/ShowKibbles'
 import About from './components/About'
-
+          // <Route exact path="/" render={() => <Home />} />
 const App = (props) => {
 	return (
 		<div>
 			<Router>
 				<Navigation />
 				Kibble
-				<AddKibbleForm />
-				<ShowKibbles />
-				<About />
+				<Route exact path="/" render={()=><ShowKibbles />}/>
+				<Route path="/addkibble" render={()=><AddKibbleForm />} />
+				<Route path="/about" render={()=><About />} />
+				<Route path="/login" render={()=><ShowKibbles />} />
 			</Router>	
 		</div>
 	)
