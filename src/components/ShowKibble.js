3423b9c7eb5477582bf763de41f5da209dfc67ac
vsparrow.com <khan@vsparrow.com>
2020-02-04 01:55:32 +0000

    grab kibble data from DB and render

--- src/components/ShowKibble.js
+++ src/components/ShowKibble.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const ShowKibble = props => {
+	const [kibbles, setKibbles] = useState([])
+	
+	useEffect(()=>{
+		(async ()=>{
+			const result = await axios.get("http://korea-ide-api-3001.run.goorm.io/kibbles")
+			console.log(result.data)
+			setKibbles(result.data)
+		})()
+	},[])
+	
 	return(
 		<div>
 		 Show Kibble info here
+		<h2>kibble data</h2>
+		<ul>
+			{kibbles.map(k=><li key={k.id}>{k.date} {k.amount}</li>)}
+		</ul>	
 		</div>
 	)
 }
