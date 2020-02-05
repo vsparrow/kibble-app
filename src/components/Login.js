6bfa3e077ecd0df941b0f834fd46172c10a98984
vsparrow.com <khan@vsparrow.com>
2020-02-05 00:28:09 +0000

    update Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = props => {
+	const [username,setUsername] = useState('')
+	const [password,setPassword] = useState('')
 	
 	const handleSubmit = e => {
 		e.preventDefault()
+		const data = {username, password}
 		console.log('submit pressed')
+		console.log('data', data)
+		setUsername('')
+		setPassword('')
 	}
+	
+	const handleUsername = e => setUsername(e.target.value)
+	const handlePassword = e => setPassword(e.target.value)
 	
 	return(
 		<div>
 			<form onSubmit={handleSubmit}>
 				<div>
 					username:
-					<input type="username"/>
+					<input type="username" value={username} onChange={handleUsername}/>
 				</div>
 				<div>
 					password:
-					<input type="password"/>
+					<input type="password" value={password} onChange={handlePassword}/>
 				</div>
 				<input type="submit"/>
 			</form>
