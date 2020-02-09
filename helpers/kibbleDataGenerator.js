09f31a0b79312b5ca9b6679043d0b6f01964fb23
vsparrow.com <khan@vsparrow.com>
2020-02-09 20:41:18 +0000

    initial Kibble data generator

---
+++ helpers/kibbleDataGenerator.js
@@ -0,0 +1,15 @@
+// This function generates kibble data until we have real data
+// Units are grams
+// Full 382 grams
+// Empty 35 grams
+// 1/2 day 46 grams
+// 1 day change 92 grams
+
+// will generate a json file for use with db.json
+// id / date / amount: number / mockdata: bool
+const generate = ()=>{
+	//create db.json object
+	const data = { "kibbles": []}
+	const id = 0
+	
+}
