0dfda4027e95d256b8dc9a830d1c480c3a1f5700
vsparrow.com <khan@vsparrow.com>
2020-02-09 21:22:33 +0000

    kibble data Generator - dates generation working

--- helpers/kibbleDataGenerator.js
+++ helpers/kibbleDataGenerator.js
@@ -7,9 +7,28 @@
 
 // will generate a json file for use with db.json
 // id / date / amount: number / mockdata: bool
+
+
+    // {
+    //   "id": 1,
+    //   "date": "2020-02-01",
+    //   "amount": "2"
+    // },
+
 const generate = ()=>{
 	//create db.json object
 	const data = { "kibbles": []}
-	const id = 0
-	
+	//starting data
+	let id = 0
+	let date = new Date(2020,1,7)//feb 7 2 2020 //months are 0-11
+	let amount = 382
+	for(i=1;i<=90;i++){
+		let newDay = { 
+			"id": ++id,
+			data: new Date( date.getFullYear(), date.getMonth(), date.getDate() - i )			
+		}
+		data["kibbles"].push(newDay)
+	}
+	console.log(data)
 }
+generate()
