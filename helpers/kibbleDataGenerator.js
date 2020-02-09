c6122c6712ae3a2ab6d85d8f3c5ca91774539f68
vsparrow.com <khan@vsparrow.com>
2020-02-09 23:16:18 +0000

    add key mockData so we can filter generated data  out in future

--- helpers/kibbleDataGenerator.js
+++ helpers/kibbleDataGenerator.js
@@ -7,27 +7,27 @@ const path = require('path')
 // 1/2 day 46 grams
 // 1 day change 92 grams
 
-// will generate a json file for use with db.json
-// id / date / amount: number / mockdata: bool
+// will generate a json file for use with json-server
+
 
 const generate = ()=>{
-	//create db.json object
+
 	const data = { "kibbles": []}
-	//starting data
 	let id = 0
-	let date = new Date(2020,1,7)//feb 7 2 2020 //months are 0-11
+	let date = new Date(2020,1,7) //months are 0-11
 	let amount = 382
 	let counter = 0; let newDate; let newDay;
+
 	while(counter <90){
 		newDate = new Date( date.getFullYear(), date.getMonth(), date.getDate() - counter )	
 		amount = amount -92 //average day change of kibble
-		newDay = { "id": ++id,date: newDate, amount , }
+		newDay = { "id": ++id,date: newDate, amount , mockData: true}
 		data["kibbles"].push(newDay)
 		counter++
 		//this emulates refilling the kibble bowl
 		if(amount < 92){
 			amount = 392
-			newDay = { "id": ++id,date: newDate, amount	}
+			newDay = { "id": ++id,date: newDate, amount , mockData: true}
 			data["kibbles"].push(newDay)
 		}
 	}
