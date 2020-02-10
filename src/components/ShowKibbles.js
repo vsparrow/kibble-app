defa0d560236e7a9a1da26a7b81a7d1322a447e9
vsparrow.com <khan@vsparrow.com>
2020-02-10 01:04:30 +0000

    expanding showKibbles

--- src/components/ShowKibbles.js
+++ src/components/ShowKibbles.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 
+
+
 const ShowKibble = props => {	
+	const [selection, setSelection] = useState("raw")
+	
+	//extract to sub componenets
+	const getRaw = data => 	{
+		return(
+			<ul>
+				{data.sort((a,b)=>b.id-a.id).map(k=><li key={k.id}>{k.date} {k.amount}</li>)}
+			</ul>)
+	}
+	const displaySelection = () => {
+		switch(selection){
+			case "raw":
+				return getRaw(props.kibbles)
+			case "daily-text":
+				return "RENDER HERE"
+		}
+	}
+	
+	const handleChange = event => setSelection(event.target.value)
+	
 	return(
 		<div>
 		 Show Kibble info here
 		<h2>kibble data</h2>
-		<ul>
-			{props.kibbles.sort((a,b)=>b.id-a.id).map(k=><li key={k.id}>{k.date} {k.amount}</li>)}
-		</ul>	
+			<select onChange={handleChange}>
+				<option value="raw" >Raw Data</option>
+				<option value="daily-text" >Daily amount consumed - text</option>
+			</select>	
+			{displaySelection()}
 		</div>
 	)
 }
